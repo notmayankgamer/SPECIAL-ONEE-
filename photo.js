85fc343f8f9c2796c8379eaacb720ecba6a29a16
notmayankgamer <chandwanimayank42@gmail.com>
2025-03-15 04:41:52 +0530

    Add files via upload

---
+++ photo.js
@@ -0,0 +1,8 @@
+
+function openLoveBox() {
+    document.getElementById("loveBox").style.display = "block";
+}
+
+function closeLoveBox() {
+    document.getElementById("loveBox").style.display = "none";
+}
